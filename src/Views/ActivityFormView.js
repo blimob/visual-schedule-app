9794da3f71387f84b4380e7ebce20f097a932036
BlineraMoberg <bm222mr@student.lnu.se>
2025-10-15 12:29:10 +0200

    Add activity modal and form: implement modal styles, structure form fields, and enhance ActivityFormView for handling activity submissions

--- src/Views/ActivityFormView.js
+++ src/Views/ActivityFormView.js
@@ -1,3 +1,55 @@
-class activityFormView {
-    constructor() {
+export class ActivityFormView {
+  #model
+  #form
+  #onSubmitCallback
+
+  constructor() {
+    this.#model = document.getElementById('activity-model')
+    this.#form = document.getElementById('activity-form')
+    this.#setupEventListeners()
+  }
+
+  show(date) {
+    this.#form.dataset.date = date.toISOString()
+
+    this.#model.classList.remove('hidden')
+  }
+
+  hide() {
+    this.#model.classList.add('hidden')
+    this.#form.reset()
+  }
+
+  onSubmit(callback) {
+    this.#onSubmitCallback = callback
+  }
+
+  #setupEventListeners() {
+    this.#form.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      const formData = {
+        date: new Date(this.#form.dataset.date),
+        name: document.getElementById('activity-name').value,
+        startTime: document.getElementById('activity-start-time').value,
+        endTime: document.getElementById('activity-end-time').value,
+        icon: document.getElementById('activity-icon').value,
+      }
+      if (this.#onSubmitCallback) {
+        this.#onSubmitCallback(formData)
+      }
+
+      const cancelbutton = this.#model.querySelector('.cancel-button')
+      cancelbutton.addEventListener('click', () => this.hide())
+    })
+
+    const closebutton = this.#model.querySelector('.close-button')
+    closebutton.addEventListener('click', () => this.hide())
+  }
+
+  this.#model.addEventListener('click', (e) => {
+    if (e.target === this.#model) {
+      this.hide()
+    }
+  })
 }

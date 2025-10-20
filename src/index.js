9391e0941a19710197b9893641591ec25ead86b8
BlineraMoberg <bm222mr@student.lnu.se>
2025-10-20 08:45:16 +0200

    Fix import paths in HTML and JavaScript files to use relative paths

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
-import { ScheduleManager } from '../src/models/ScheduleManager.js'
-import { CalendarModel } from '../src/models/CalendarModel.js'
-import { CalendarController } from '../src/controllers/CalendarController.js'
-import { MonthView } from '../src/Views/MonthView.js'
-
-// Allt med RELATIVA paths!
+import { ScheduleManager } from './models/ScheduleManager.js'
+import { CalendarModel } from './models/CalendarModel.js'
+import { CalendarController } from './controllers/CalendarController.js'
+import { MonthView } from './Views/MonthView.js'
 
 console.log('🚀 Visual Schedule App Starting...')
 
-// Skapa Models
 const scheduleManager = new ScheduleManager()
 const calendarModel = new CalendarModel()
 
-// Skapa Views
 const calendarContainer = document.getElementById('calendar-container')
 const monthView = new MonthView(calendarContainer)
 
-// Skapa Controller
 const calendarController = new CalendarController(
   scheduleManager,
   calendarModel,
   monthView
 )
 
-// Starta appen!
 calendarController.initialize()
 
 console.log('✅ App initialized!')

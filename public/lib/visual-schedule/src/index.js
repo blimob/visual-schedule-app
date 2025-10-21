f845cebf98a20d209ea80c239acf4dbe57354e49
BlineraMoberg <bm222mr@student.lnu.se>
2025-10-21 08:17:22 +0200

    Remove visual-schedule library and related files from the project

--- public/lib/visual-schedule/src/index.js
+++
@@ -1,4 +0,0 @@
-export { Activity } from './Activity.js'
-export { Child } from './Child.js'
-export { DaySchedule } from './DaySchedule.js'
-export { WEEKDAY_COLORS, getColorForDate } from './weekdayColors.js'
